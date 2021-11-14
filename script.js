1d7a2f9dfda6a8152cfaceec103a13631954279d
Aaqib2121 <aaqibnakwa4913@gmail.com>
2021-11-14 17:52:50 +0530

    DOJO-2

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:2,
+        itemsDesktop:[1199,2],
+        itemsDesktopSmall:[980,1],
+        itemsTablet:[768,1],
+        pagination:true,
+        navigation:false
+    });
+});
+
+
+
+
+
+// const scrollbtn = document.querySelector("gotopbtn");
+
+// window.addEventListener("scroll",scrollfunction);
+
+// function scrollfunction() {
+//     if(window.pageYOffset > 300){
+//         if(!scrollbtn.classList.add("gotopbtn")){
+//             scrollbtn.classList.add("gotopbtn")
+//             scrollbtn.style.display = "block";
+//         }
+        
+//     }
+//     else{
+//         if(!scrollbtn.classList.add("gotopbtn")){
+//             scrollbtn.style.display = "none";
+//         }   
+//     }
+// };
+
+// gotopbtn.addEventListener("click",gotopbtn);
+
+// function scrollbtn() {
+//     window.scrollTo(0,0);
+// }
+
  
-// $(document).ready(function(){
-//     $("#testimonial-slider").owlCarousel({
-//         items:1,
-//         itemsDesktop:[1000,1],
-//         itemsDesktopSmall:[979,1],
-//         itemsTablet:[769,1],
-//         pagination:true,
-//         autoplay:true
-//     });
-// });
+ 
